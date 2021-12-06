78e04bb7d52c727f56d6b487f086f84135bcba38
Vinícius Lucena <jviniiciuslucena@gmail.com>
2021-12-06 16:00:55 -0300

    Desafio 01 (Chapter I) resolvido :rocket:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,24 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.length === 0) return;
+
+    setTasks([...tasks, {title: newTaskTitle, isComplete: false, id: Math.random()}])
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task)
+
+    setTasks(updatedTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
